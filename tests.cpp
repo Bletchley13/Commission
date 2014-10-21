79c39504f3209cbf2a08bf9bda33ba1ad5cf4a62
bletchley <bletchley@Viking.(none)>
2014-10-21 13:32:03 +0800

    Equivalent Class Test

--- tests.cpp
+++ tests.cpp
@@ -35,6 +35,37 @@ TEST(BoundaryTest, Barrel) {
         EXPECT_FLOAT_EQ( Invalid, Commission(0,0,-2) );
 }
 
+TEST( EquivalentClassTest, LESS1000) {
+	EXPECT_FLOAT_EQ( 70.0, Commission(7,7,7) );
+	EXPECT_FLOAT_EQ( 100.0, Commission(10,10,10) );
+	EXPECT_FLOAT_EQ( 90.0, Commission(5,10,15) );
+	EXPECT_FLOAT_EQ( 90.0, Commission(20,0,0) );
+	EXPECT_FLOAT_EQ( 60.0, Commission(0,20,0) );
+}
+
+TEST( EquivalentClassTest, MORE1000LESS1800) {
+	EXPECT_FLOAT_EQ( 220.0, Commission(10,20,30) );
+	EXPECT_FLOAT_EQ( 175.0, Commission(15,15,15) );
+	EXPECT_FLOAT_EQ( 115.0, Commission(15,10,5) );
+	EXPECT_FLOAT_EQ( 152.5, Commission(30,0,0) );
+	EXPECT_FLOAT_EQ( 137.5, Commission(0,0,50) );
+}
+
+TEST( EquivalentClassTest, MORE1800) {
+	EXPECT_FLOAT_EQ( 1060.0, Commission(60,60,60) );
+	EXPECT_FLOAT_EQ( 260.0, Commission(20,20,20) );
+	EXPECT_FLOAT_EQ( 310.0, Commission(50,0,0) );
+	EXPECT_FLOAT_EQ( 280.0, Commission(0,70,0) );
+ 	EXPECT_FLOAT_EQ( 260.0, Commission(0,0,80) );
+}
+
+TEST( EquivalentClassTest, INVALID) {
+        EXPECT_FLOAT_EQ( Invalid, Commission(90,60,60) );
+        EXPECT_FLOAT_EQ( Invalid, Commission(20,100,20) );
+        EXPECT_FLOAT_EQ( Invalid, Commission(50,0,110) );
+        EXPECT_FLOAT_EQ( Invalid, Commission(-7,70,0) );
+        EXPECT_FLOAT_EQ( Invalid, Commission(0,0,-8) );
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
