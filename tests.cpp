e6896c42d8b0f3de9d71030f8377a3b70264fe33
bletchley <bletchley@Viking.(none)>
2014-10-21 13:10:27 +0800

    Boundary Test

--- tests.cpp
+++ tests.cpp
@@ -2,15 +2,39 @@
 #include <gtest/gtest.h>
 
 TEST(BoundaryTest, Lock) { 
-	EXPECT_EQ( 0 , Commission(0,0,0)  );
-	EXPECT_EQ( 1, Commission(1,1,1) );
-	
+	EXPECT_FLOAT_EQ( 0 , Commission(0,0,0)  );
+	EXPECT_FLOAT_EQ( 10.0, Commission(1,1,1) );
+	EXPECT_FLOAT_EQ( 4.5, Commission(1,0,0) );
+	EXPECT_FLOAT_EQ( 490.0, Commission(70,0,0) );
+	EXPECT_FLOAT_EQ( 481.0, Commission(69,0,0) );
+		
+
+	EXPECT_FLOAT_EQ( Invalid, Commission(71,0,0) );
+	EXPECT_FLOAT_EQ( Invalid, Commission(-2,0,0) );
 }
 
 TEST(BoundaryTest, Stack) {
-	
+        EXPECT_FLOAT_EQ( 3.0, Commission(0,1,0) );
+        EXPECT_FLOAT_EQ( 340.0, Commission(0,80,0) );
+        EXPECT_FLOAT_EQ( 334.0, Commission(0,79,0) );
+
+
+        EXPECT_FLOAT_EQ( Invalid, Commission(0,81,0) );
+	EXPECT_FLOAT_EQ( Invalid, Commission(0,-2,0) );
+        EXPECT_FLOAT_EQ( Invalid, Commission(0,-1,0) );
 }
  
+TEST(BoundaryTest, Barrel) {
+        EXPECT_FLOAT_EQ( 2.5, Commission(0,0,1) );
+        EXPECT_FLOAT_EQ( 310.0, Commission(0,0,90) );
+        EXPECT_FLOAT_EQ( 305.0, Commission(0,0,89) );
+
+
+        EXPECT_FLOAT_EQ( Invalid, Commission(0,0,91) );
+        EXPECT_FLOAT_EQ( Invalid, Commission(0,0,-1) );
+        EXPECT_FLOAT_EQ( Invalid, Commission(0,0,-2) );
+}
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
